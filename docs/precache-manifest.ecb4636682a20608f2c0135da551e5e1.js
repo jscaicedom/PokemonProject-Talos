b55cce479a721a47f9098b5ad97617a4b41f7265
Johana Sarai Caicedo <johanasaraicaicedo@Ricardos-MacBook-Pro.local>
2020-09-28 11:53:59 -0500

    docs

---
+++ docs/precache-manifest.ecb4636682a20608f2c0135da551e5e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823fd360c48fff4721e5a6ec2a664020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9104a8bc067eaa856ee",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "07aa28fb838b2f00f916",
+    "url": "/static/css/main.68ea3612.chunk.css"
+  },
+  {
+    "revision": "b9104a8bc067eaa856ee",
+    "url": "/static/js/2.1a408254.chunk.js"
+  },
+  {
+    "revision": "aebc32425105075f04663527fb2971ca",
+    "url": "/static/js/2.1a408254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07aa28fb838b2f00f916",
+    "url": "/static/js/main.495896f3.chunk.js"
+  },
+  {
+    "revision": "7ca482102f47c9576585",
+    "url": "/static/js/runtime-main.3395de5f.js"
+  },
+  {
+    "revision": "c405f9ebb67cb4e28fbcaf24db2cb1cb",
+    "url": "/static/media/portada-pokemon.c405f9eb.jpg"
+  }
+]);
