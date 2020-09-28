4d12c965d0bc16d8d402148237d098752dc1367e
Johana Sarai Caicedo <johanasaraicaicedo@Ricardos-MacBook-Pro.local>
2020-09-28 12:12:44 -0500

    Updates

---
+++ precache-manifest.821966b8ea2dce40bab55faaeb8c0d57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7047bad3fc7c39b0e596ac9a1b2588",
+    "url": "/PokemonProject-Talos/index.html"
+  },
+  {
+    "revision": "0eda79117de446237c38",
+    "url": "/PokemonProject-Talos/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f4850f638d8b188c007f",
+    "url": "/PokemonProject-Talos/static/css/main.e4db1a5e.chunk.css"
+  },
+  {
+    "revision": "0eda79117de446237c38",
+    "url": "/PokemonProject-Talos/static/js/2.2e451679.chunk.js"
+  },
+  {
+    "revision": "aebc32425105075f04663527fb2971ca",
+    "url": "/PokemonProject-Talos/static/js/2.2e451679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4850f638d8b188c007f",
+    "url": "/PokemonProject-Talos/static/js/main.829ec092.chunk.js"
+  },
+  {
+    "revision": "119a1f0d73c422770e0d",
+    "url": "/PokemonProject-Talos/static/js/runtime-main.38776c72.js"
+  },
+  {
+    "revision": "c405f9ebb67cb4e28fbcaf24db2cb1cb",
+    "url": "/PokemonProject-Talos/static/media/portada-pokemon.c405f9eb.jpg"
+  }
+]);
